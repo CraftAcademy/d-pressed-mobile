1d03fa8df483c722b6985f6cc722b8af5d0ca12e
AndreasMN <andycutter@godmail.dk>
2018-08-31 15:20:02 +0200

    add 0,5 second before test clicks on menu button. Now test should pass every time

--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -24,23 +24,21 @@ export class Page {
 
   fillInForm(email, password) {
     element(by.css('.bar-button-menutoggle')).click()
+    browser.driver.sleep(500)
     .then(() => {
-
       element.all(by.css('.toolbar-title')).first().getText().then(text => expect(text).toEqual('Menu'));
       browser.driver.sleep(500);
       element(by.buttonText('Login')).click().then(() => {
-        browser.driver.sleep(1000);
+        browser.driver.sleep(500);
         element.all(by.className('alert-input')).then((items) => {
           items[0].sendKeys(email);
           items[1].sendKeys(password);
         });
       })
+      browser.driver.sleep(500);
       element(by.cssContainingText('.button-inner', 'Login')).click();
       browser.driver.sleep(2000); 
-      
     })};
-
-
 
     results_card_content() {
       return element(by.css('ion-card-header')).getText();
